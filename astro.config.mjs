cb25593dc359951143db57ed5ac76d3f5cb72846
Sean Lange <seanlange74@gmail.com>
2026-04-03 21:57:14 -0400

    fixed it

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
-// @ts-check
-import node from '@astrojs/node';
 import { defineConfig } from 'astro/config';
+
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-	output: 'server',
-	adapter: node({
-		mode: 'standalone',
-	}),
-	server: {
-		host: true,
-	},
+    output: 'server',
+    adapter: vercel(),
+    server: {
+        host: true,
+    },
 });
